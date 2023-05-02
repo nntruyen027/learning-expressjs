327306a3a50c99407f573766f44f9a7bee6c4d89
Nguyễn Ngọc Truyện <truyen027@gmail.com>
2023-05-02 17:27:24 +0700

    form

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const express = require('express');
-const user = require('./userRouter');
+const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
 
-let app = express();
+const storage = multer.diskStorage({
+    destination: (req, res, callback) => {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: (req, file, callback) => {
+        callback(null, Date.now() + "_" + file.originalname);
+    }
+});
 
-app.use('/user', user);
+const upload = multer({ storage: storage });
 
-app.listen(3000)
+//Khai báo middleware
+
+//khai báo static folder
+app.use(express.static('./uploads'));
+
+//khai báo thư mục đên các đường dẫn của template
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+//parsing application/json
+app.use(bodyParser.json());
+
+//parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render("form");
+});
+
+app.post('/', upload.single("avatar"), (req, res) => {
+    const { body, file } = req;
+    console.log(file);
+    res.render("info", { body, file });
+});
+
+app.listen(8080);
